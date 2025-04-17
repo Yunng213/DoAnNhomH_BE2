6bda8919bdba8d07e48e15f2959c1521f8d081d6
Yunng213 <dungntt213@gmail.com>
2025-04-17 12:28:24 +0700

    fix map link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -181,7 +181,8 @@
                         <h2 class="footer-wid-title" style="text-align: center;">Map</h2>
                     </div>
                     <div class="footer-map">
-                        <iframe src="https://www.google.com/maps?q=Điện+thoại+di+động+Ustora,+21/2A+Phan+Huy+Ích,+Phường+12,+Gò+Vấp,+Thành+phố+Hồ+Chí+Minh,+Việt+Nam&output=embed" width="100%" height="260" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d368.837856855349!2d106.76902352735392!3d10.88098760860244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d90022426c6d%3A0x438eaf5d0afb4c6!2zxJBp4buHbiB04butIFRlY2hab25l!5e0!3m2!1sen!2s!4v1744867595402!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <!-- <iframe src="https://www.google.com/maps?q=Điện+thoại+di+động+Ustora,+21/2A+Phan+Huy+Ích,+Phường+12,+Gò+Vấp,+Thành+phố+Hồ+Chí+Minh,+Việt+Nam&output=embed" width="100%" height="260" frameborder="0" style="border:0" allowfullscreen=""></iframe> -->
                     </div>
                 </div>
             </div>
@@ -192,7 +193,7 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="copyright">
-                        <p>&copy; 2024 All Rights Reserved.</p>
+                        <p>&copy; 2025 All Rights Reserved.</p>
                     </div>
                 </div>
 
